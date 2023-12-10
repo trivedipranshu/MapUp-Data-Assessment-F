c70dde7f17ba1a927fef7813e8ffa469bcda02f1
Pranshu Trivedi <144879589+trivedipranshu@users.noreply.github.com>
2023-12-10 20:03:37 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 
 def generate_car_matrix(df)->pd.DataFrame:
-    """
+    car_matrix = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
+    car_matrix.values[[range(len(car_matrix))]*2] = 0  # Set diagonal values to 0
+    return car_matrix
     Creates a DataFrame  for id combinations.
 
     Args:
@@ -18,7 +20,9 @@ def generate_car_matrix(df)->pd.DataFrame:
 
 
 def get_type_count(df)->dict:
-    """
+    df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')], labels=['low', 'medium', 'high'])
+    type_count = df['car_type'].value_counts().to_dict()
+    return dict(sorted(type_count.items()))
     Categorizes 'car' values into types and returns a dictionary of counts.
 
     Args:
@@ -33,7 +37,9 @@ def get_type_count(df)->dict:
 
 
 def get_bus_indexes(df)->list:
-    """
+    mean_bus = df['bus'].mean()
+    bus_indexes = df[df['bus'] > 2 * mean_bus].index.sort_values().tolist()
+    return bus_indexes
     Returns the indexes where the 'bus' values are greater than twice the mean.
 
     Args:
@@ -47,8 +53,9 @@ def get_bus_indexes(df)->list:
     return list()
 
 
-def filter_routes(df)->list:
-    """
+def filter_routes(df)->list
+    avg_truck_routes = df[df['truck'] > 7]['route'].unique()
+    return sorted(avg_truck_routes.tolist())
     Filters and returns routes with average 'truck' values greater than 7.
 
     Args:
@@ -63,7 +70,8 @@ def filter_routes(df)->list:
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
-    """
+    modified_matrix = car_matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25).round(1)
+    return modified_matrix
     Multiplies matrix values with custom conditions.
 
     Args:
@@ -78,7 +86,14 @@ def multiply_matrix(matrix)->pd.DataFrame:
 
 
 def time_check(df)->pd.Series:
-    """
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['day_of_week'] = df['timestamp'].dt.day_name()
+    df['hour'] = df['timestamp'].dt.hour
+
+    time_check_series = df.groupby(['id', 'id_2'])['hour'].nunique() == 24
+    day_check_series = df.groupby(['id', 'id_2'])['day_of_week'].nunique() == 7
+
+    return time_check_series & day_check_series
     Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
 
     Args:
@@ -90,3 +105,9 @@ def time_check(df)->pd.Series:
     # Write your logic here
 
     return pd.Series()
+    # Example usage:
+# df1 = pd.read_csv('dataset-1.csv')
+# result1 = generate_car_matrix(df1)
+
+# df2 = pd.read_csv('dataset-2.csv')
+# result2 = time_check(df2)
